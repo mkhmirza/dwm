444a90cfe139d5d2ea0089bdcde99b429f950d6a
Kumail Habib <kumailhabib12@gmail.com>
2021-10-22 19:04:50 +0500

    Changed dmenu colors

--- config.def.h
+++ config.def.h
@@ -19,6 +19,8 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#212121";
 static const char col_dark[]        = "#424242";
+/* dmenu color schemes */
+static const char selectedBack[] = "#0D47A1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -69,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", selectedBack, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 // firefox
 static const char *firefox[] = {"firefox", NULL};
