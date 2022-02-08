0853edd4668c3cb06c6d19271f8a61c2d331e788
Kumail Habib <kumailhabib12@gmail.com>
2022-02-08 21:54:12 +0500

    chenges

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=13" };
+static const char dmenufont[]       = "monospace:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -76,8 +76,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 // firefox
 static const char *firefox[] = {"firefox", NULL};
-// transmission-qt 
-static const char *transmissionQt[] = {"transmission-qt", NULL};
+// transmission
+static const char *transmissionQt[] = {"transmission", NULL};
 // pavucontrol
 static const char *pavucontrol[] = {"pavucontrol", NULL};
 
